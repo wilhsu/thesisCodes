a97bd33b04fb3abff557362ff1a0ce8cf08c0cb0
wilhsu <80747302+wilhsu@users.noreply.github.com>
2023-10-10 23:09:36 -0400

    fkdjsklfdslkj

---
+++ js/index.js
@@ -0,0 +1,23 @@
+var Airtable = require('airtable');
+var base = new Airtable({apiKey: 'patpQilcK7ah8GBip.de1a807d9e1ac1201aa443d7968212f2d1953505d2407a602eb70e468bc51f0e'}).base('app2jiKwvykQ53cAK');
+
+base('256').select({
+    // maxRecords: 3,
+    view: "Grid view"
+}).eachPage(async function page(records, fetchNextPage) {
+
+
+    records.forEach(function(record) {
+        console.log(record.get('Images')[0]);
+        // document.querySelector('#big').
+
+        let image = document.createElement("img");
+	      image.classList.add("img");
+	      image.src = record.get('Images')[0].url;
+	      document.querySelector("#big").append(image);
+    });
+
+    fetchNextPage();
+}, function done(err) {
+    if (err) { console.error(err); return; }
+});
